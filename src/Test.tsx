b15f0f7a54ae9a0e44c72d1caee8cc88f36fc013
gogy9000 <goropashnyj9000@gmail.com>
2022-07-22 19:53:40 +0500

    commit-hyit

--- src/Test.tsx
+++ src/Test.tsx
@@ -1,12 +1,12 @@
 import SuperCheckbox from "./component/super-checkbox/SuperCheckbox";
 import SuperInputText from "./component/super-input/SuperInput";
-import { ButtonStyled } from "./styled-components/StyledButtons";
+import {SuperButton} from "./component/super-button/SuperButton";
 
 export const Test = () => {
     return (
         <>
             <SuperInputText/>
-            <ButtonStyled red>red</ButtonStyled>
+            <SuperButton/>
             <SuperCheckbox/>
         </>
     )
--- src/component/super-button/SuperButton.tsx
+++ src/component/super-button/SuperButton.tsx
@@ -1,11 +1,10 @@
 import React, {ButtonHTMLAttributes, DetailedHTMLProps} from 'react'
-import styled from "styled-components";
 
 
 // тип пропсов обычной кнопки, children в котором храниться название кнопки там уже описан
 type DefaultButtonPropsType = DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>
 
-type SuperButtonPropsType = DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>& {
+type SuperButtonPropsType =DefaultButtonPropsType& {
     red?: boolean
 }
 
@@ -13,23 +12,17 @@ type SuperButtonPropsType = DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonEle
 export const SuperButton: React.FC<SuperButtonPropsType> = (
     {
         red,
+        className,
         ...restProps// все остальные пропсы попадут в объект restProps, там же будет children
     }
 ) => {
-    // const s ={
-    //     red:'red',
-    //     default:{}
-    // }
+
     // const finalClassName = `${red ? s.red : s.default} ${className}`
 
-    //
-    //
-    //
+
     return (
         <>
-            {/*@ts-ignore*/}
             <button
-
                 {...restProps} // отдаём кнопке остальные пропсы если они есть (children там внутри)
             />
         </>
